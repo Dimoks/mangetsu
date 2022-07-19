0ed86e2a73489722bd6ce900beffb8593c047a28
bilditup1 <357902+bilditup1@users.noreply.github.com>
2022-07-19 16:06:34 -0400

    Removed SKIP_INSTALL_ALL guard; more comments; cleanup

--- cmake/install_cfg.cmake
+++ cmake/install_cfg.cmake
@@ -1,63 +1,59 @@
 #
-# Installation configuration
+# install_cfg.cmake: Installation configuration
 #
-if (NOT SKIP_INSTALL_ALL)
-    #
-    # Configure install destinations
-    #
-    if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT AND ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
-        set(CMAKE_INSTALL_PREFIX $ENV{LOCALAPPDATA} CACHE PATH "Installation prefix on Windows." FORCE)
-        set(INSTALL_BIN_DIR "${CMAKE_INSTALL_PREFIX}/${CMAKE_PROJECT_NAME}" CACHE PATH "Installation directory for executables")
-    else()
-        set(INSTALL_BIN_DIR "${CMAKE_INSTALL_PREFIX}/bin" CACHE PATH "Installation directory for executables")
-    endif()
-    #
-    # Actually add install destination for each binary
-    #
-    get_property(_TARGETS_LIST
-        DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} 
-        PROPERTY BUILDSYSTEM_TARGETS
-    )
-    foreach (_target ${_TARGETS_LIST})
-        get_target_property(_target_type ${_target} TYPE)
-        #
-        # Only install shared libraries and executables, not static libraries.
-        #
-         if (${_target_type} STREQUAL "EXECUTABLE" OR 
-             ${_target_type} STREQUAL "SHARED_LIBRARY")
-            install( TARGETS ${_target}
-                CONFIGURATIONS Release 
-                RUNTIME DESTINATION "${INSTALL_BIN_DIR}"
-            )
-         endif()
-        #
-        # Strip built files of everything except relocation information
-        # when building for Release. 
-        # ref1: https://www.technovelty.org/linux/stripping-shared-libraries.html
-        # ref2: https://stackoverflow.com/a/14176386/
-        # src:  https://stackoverflow.com/a/53692364/
-        #
-        # if statement added to filter out the mman-win32 ExternalProject.
-        #
-        if (${_target_type} STREQUAL "EXECUTABLE" OR 
-            ${_target_type} STREQUAL "SHARED_LIBRARY" OR
-            ${_target_type} STREQUAL "STATIC_LIBRARY")
-            add_custom_command(
-               TARGET ${_target} DEPENDS ${_target}
-               POST_BUILD
-               COMMAND $<$<CONFIG:release>:${CMAKE_STRIP}>
-               ARGS --strip-unneeded $<TARGET_FILE:${_target}>
-             )
-        endif ()
-     endforeach()
-     #
-     # Post-Install Tasks (only Windows for now)
-     # src:      https://stackoverflow.com/a/29979349/
-     #
-	 if(${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows")
-        # Make local vars available to subdirectory's CMakeLists.txt
-        install(CODE "set(WORKINGDIR \"${CMAKE_SOURCE_DIR}\")")
-        install(CODE "set(INSTALL_BIN_DIR \"${INSTALL_BIN_DIR}\")")
-        add_subdirectory(${CMAKE_SOURCE_DIR}/cmake/postinstall)
-     endif()
-endif(NOT SKIP_INSTALL_ALL)
+
+# Override default installation folder on Windows
+if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT AND WIN32)
+  set(CMAKE_INSTALL_PREFIX "$ENV{LOCALAPPDATA}" CACHE PATH "Installation prefix on Windows." FORCE)
+  set(INSTALL_BIN_DIR "${CMAKE_INSTALL_PREFIX}/${CMAKE_PROJECT_NAME}" CACHE PATH "Installation directory for executables")
+else()
+  set(INSTALL_BIN_DIR "${CMAKE_INSTALL_PREFIX}/bin" CACHE PATH "Installation directory for executables")
+endif()
+
+# Get list of targets
+get_property(_TARGETS_LIST
+    DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} 
+    PROPERTY BUILDSYSTEM_TARGETS
+)
+# Loop through target list
+foreach (_target ${_TARGETS_LIST})
+  # Get type of target (executable, shared lib, static lib)
+  get_target_property(_target_type ${_target} TYPE)
+  #
+  # Strip built files of everything except relocation information
+  # when building for Release. 
+  # ref1: https://www.technovelty.org/linux/stripping-shared-libraries.html
+  # ref2: https://stackoverflow.com/a/14176386/
+  # src:  https://stackoverflow.com/a/53692364/
+  #
+  # if() added to prevent mman-win32 ExternalProject from being evaluated
+  #
+  if (${_target_type} STREQUAL "EXECUTABLE" OR 
+      ${_target_type} STREQUAL "SHARED_LIBRARY" OR
+      ${_target_type} STREQUAL "STATIC_LIBRARY")
+         add_custom_command(
+            TARGET ${_target} DEPENDS ${_target}
+            POST_BUILD
+            COMMAND $<$<CONFIG:release>:${CMAKE_STRIP}>
+            ARGS --strip-unneeded $<TARGET_FILE:${_target}>
+          )
+  endif()
+  # Only install shared libraries and executables, not static libraries.
+  if (${_target_type} STREQUAL "EXECUTABLE" OR 
+      ${_target_type} STREQUAL "SHARED_LIBRARY")
+      install( TARGETS ${_target}
+         CONFIGURATIONS Release 
+         RUNTIME DESTINATION "${INSTALL_BIN_DIR}"
+      )
+  endif()
+endforeach()
+#
+# Post-Install Tasks (only Windows for now)
+# src: https://stackoverflow.com/a/29979349/
+#
+if(WIN32)
+# Make local vars available to subdirectory's CMakeLists.txt
+  install(CODE "set(WORKINGDIR \"${CMAKE_SOURCE_DIR}\")")
+  install(CODE "set(INSTALL_BIN_DIR \"${INSTALL_BIN_DIR}\")")
+  add_subdirectory(${CMAKE_SOURCE_DIR}/cmake/postinstall)
+endif()
