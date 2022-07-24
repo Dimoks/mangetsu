7df8b800bf8fcd9cfb79f52e874f05d35c484856
bilditup1 <357902+bilditup1@users.noreply.github.com>
2022-07-24 03:02:35 -0400

    Initial packaging support (Windows)

--- cmake/Install.cmake
+++ cmake/Install.cmake
@@ -5,9 +5,9 @@
 # Override default installation folder on Windows
 if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT AND WIN32)
   set(CMAKE_INSTALL_PREFIX "$ENV{LOCALAPPDATA}" CACHE PATH "Installation prefix on Windows." FORCE)
-  set(INSTALL_BIN_DIR "${CMAKE_INSTALL_PREFIX}/${CMAKE_PROJECT_NAME}" CACHE PATH "Installation directory for executables")
+  set(INSTALL_BIN_DIR "${CMAKE_PROJECT_NAME}" CACHE PATH "Installation directory for executables")
 else()
-  set(INSTALL_BIN_DIR "${CMAKE_INSTALL_PREFIX}/bin" CACHE PATH "Installation directory for executables")
+  set(INSTALL_BIN_DIR "bin" CACHE PATH "Installation directory for executables")
 endif()
 
 # Get list of targets
@@ -37,7 +37,7 @@ foreach (_target ${_TARGETS_LIST})
             COMMAND $<$<CONFIG:release>:${CMAKE_STRIP}>
             ARGS --strip-unneeded $<TARGET_FILE:${_target}>
           )
-  endif()
+   endif()
   # Only install shared libraries and executables, not static libraries.
   if (${_target_type} STREQUAL "EXECUTABLE" OR 
       ${_target_type} STREQUAL "SHARED_LIBRARY")
@@ -54,6 +54,6 @@ endforeach()
 if(WIN32)
 # Make local vars available to subdirectory's CMakeLists.txt
   install(CODE "set(WORKINGDIR \"${CMAKE_SOURCE_DIR}\")")
-  install(CODE "set(INSTALL_BIN_DIR \"${INSTALL_BIN_DIR}\")")
-  add_subdirectory(${CMAKE_SOURCE_DIR}/cmake/postinstall)
+  install(CODE "set(INSTALL_BIN_DIR \"${CMAKE_INSTALL_PREFIX}/${INSTALL_BIN_DIR}\")")
+  add_subdirectory("${CMAKE_SOURCE_DIR}/cmake/postinstall")
 endif()
